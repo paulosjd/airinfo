1a4f3cd032af593e9c8191efaba0074a1b9007ec
Paul Davis <pjdavis@gmx.com>
2019-03-03 21:55:14 +0000

    work on heatmaps

--- src/components/air_calendars.js
+++ src/components/air_calendars.js
@@ -1,5 +1,6 @@
 import React from "react";
 import CalendarHeatmap from 'react-calendar-heatmap';
+import HeatmapCalendar from './heatmap_calendar'
 
 import 'react-calendar-heatmap/dist/styles.css';
 
@@ -17,7 +18,7 @@ class AirCalendars extends React.Component {
 
     getCalendarData() {
         let data = null;
-        const url = 'http://api.air-aware.com/stats/highest-sites/'.concat('pm10/','200');
+        const url = 'http://api.air-aware.com/stats/highest-sites/'.concat('pm10/','500');
         fetch(url)
             .then(response => response.json())
             .then(aqData => {this.setState({calendarData: aqData})});
@@ -35,34 +36,35 @@ class AirCalendars extends React.Component {
 
     render() {
         const data = this.state.calendarData;
-        const rows2 = []
+        const rows2 = [];
         data.forEach((value, i) => {
             if ( i % 2 === 0 ) {
-                rows2.push(
-                    <tr>
-                        <td><h5 className='heatmap_label'>{value.name}</h5></td>
-                        <td><h5 className='heatmap_label'>{data[i + 1] ? data[i + 1].name : ''}</h5></td>
-                    </tr>);
-                const date_vals_1 = [];
-                const date_vals_2 = [];
-                for (let [key, val] of Object.entries(value.date_counts)) {
-                        date_vals_1.push({date: new Date(key), count: val})
+                if (data[i + 1]) {
+                    rows2.push(
+                        <tr key={i}>
+                            <td><h5 className='heatmap_label'>{value.name}</h5></td>
+                            <td><h5 className='heatmap_label'>{data[i + 1] ? data[i + 1].name : ''}</h5></td>
+                        </tr>);
                 }
-
+                const date_vals = [[], []];
+                for (let [key, val] of Object.entries(value.date_counts)) {
+                        date_vals[0].push({date: new Date(key), count: val})
+                }
                 if ( data[i + 1]) {
                     for (let [key, val] of Object.entries(data[i + 1].date_counts)) {
-                            date_vals_2.push({date: new Date(key), count: val})
+                            date_vals[1].push({date: new Date(key), count: val})
                     }
                 }
-                console.log(date_vals_1)
-                console.log(date_vals_2)
-                rows2.push(
-                    <tr>
+                if (date_vals[1].length > 0){
+                rows2.push(<><tr key={i + 'cal-key'}>
+                    <td><HeatmapCalendar dateCounts={date_vals[0]} />
+                    <td><HeatmapCalendar dateCounts={date_vals[1]} />
+                        </td>
                         <td>
                             <CalendarHeatmap
-                                startDate={shiftDate(today, -150)}
+                                numDays={120}
                                 endDate={today}
-                                values={date_vals_1}
+                                values={date_vals[1]}
                                 classForValue={(value) => {
                                     if (!value) {
                                         return 'color-empty';
@@ -72,44 +74,22 @@ class AirCalendars extends React.Component {
                                 showWeekdayLabels={true}
                             />
                         </td>
-                        <td>
-                            <CalendarHeatmap
-                                startDate={shiftDate(today, -150)}
-                                endDate={today}
-                                values={date_vals_1}
-                                classForValue={(value) => {
-                                    if (!value) {
-                                        return 'color-empty';
-                                    }
-                                    return `color-scale-${value.count}`;
-                                }}
-                                showWeekdayLabels={true}
-                            />
-                        </td>
-                        </tr>)
-            }
+                        </tr></>)
+            }}
         });
-
         if ( this.state.calendarData.length > 0 ) {return (
             <>
+            <div className="btn-group">
+                <button className='calendar-option'>Test button 1</button>
+                <button className='calendar-option'>Test button 1</button>
+                <button className='calendar-option'>Test button 1</button>
+            </div>
             <table>
-                <thead>
-                <tr>
-                </tr>
-                </thead>
-                <tbody>
-                {rows2}
-                </tbody>
+                <tbody>{rows2}</tbody>
             </table>
             </>
         )} return (<p className='loading_text'>Loading...</p>)
     }
-}
-
-function shiftDate(date, numDays) {
-    const newDate = new Date(date);
-    newDate.setDate(newDate.getDate() + numDays);
-    return newDate;
 }
 
 export default AirCalendars;
--- src/components/heatmap_calendar.js
+++ src/components/heatmap_calendar.js
@@ -1,71 +1,21 @@
-import CalendarHeatmap from "./air_calendars";
 import React from "react";
+import CalendarHeatmap from 'react-calendar-heatmap';
 
 const today = new Date();
 
-const SiteCatRow = (props) => {
-    return <tr><th colSpan="2">{props.category}</th></tr>;
-};
-
-const HeatmapCalendars = (props) => {
-
-    const handleClick = (e) => {
-        e.preventDefault();
-    };
-
+export default (props) => {
     return (
-        <>)
-            <td>
-                <CalendarHeatmap
-                    startDate={shiftDate(today, -150)}
-                    endDate={today}
-                    values={[
-                        {date: '2019-01-01', count: 1},
-                        {date: '2019-01-03', count: 4},
-                        {date: '2019-01-06', count: 2},
-                        {date: '2019-01-11', count: 1},
-                        {date: '2019-01-13', count: 4},
-                        {date: '2019-01-16', count: 2},
-                        // ...and so on
-                    ]}
-                    classForValue={(value) => {
-                        if (!value) {
-                            return 'color-empty';
-                        }
-                        return `color-scale-${value.count}`;
-                    }}
-                    showWeekdayLabels={true}
-                />
-            </td>
-            <td>
-                <CalendarHeatmap
-                    startDate={shiftDate(today, -150)}
-                    endDate={today}
-                    values={[
-                        {date: '2019-01-01', count: 1},
-                        {date: '2019-01-03', count: 4},
-                        {date: '2019-01-06', count: 2},
-                        {date: '2019-01-11', count: 1},
-                        {date: '2019-01-13', count: 4},
-                        {date: '2019-01-16', count: 2},
-                        // ...and so on
-                    ]}
-                    classForValue={(value) => {
-                        if (!value) {
-                            return 'color-empty';
-                        }
-                        return `color-scale-${value.count}`;
-                    }}
-                    showWeekdayLabels={true}
-                />
-            </td>
-        </>);
-};
-
-function shiftDate(date, numDays) {
-    const newDate = new Date(date);
-    newDate.setDate(newDate.getDate() + numDays);
-    return newDate;
+        <CalendarHeatmap
+            numDays={120}
+            endDate={today}
+            values={props.dateCounts}
+            classForValue={(value) => {
+                if (!value) {
+                    return 'color-empty';
+                }
+                return `color-scale-${value.count}`;
+            }}
+            showWeekdayLabels={true}
+        />
+    )
 }
-
-export default HeatmapCalendars;
